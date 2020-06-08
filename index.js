1c5552ad73f69208992b7251997f777e8d824058
sara altaee <sara.korea96@gmail.com>
2020-06-08 14:18:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(tutorial => tutorial.split(' ').map(eachWord => eachWord.charAt(0).toUpperCase() + eachWord.slice(1)).join(' '))
+
+
+console.log(titleCased);
+
